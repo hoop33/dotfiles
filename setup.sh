fbe9467da6f3268f2db0bd5da2029b912dacfb89
Rob Warner <rwarner@grailbox.com>
2015-02-02 10:58:44 -0500

    Add YouCompleteMe configuration

--- setup.sh
+++ setup.sh
@@ -9,5 +9,6 @@ ln -fs $DOTFILES/.gitignore ~/.gitignore
 ln -fs $DOTFILES/.vimrc ~/.vimrc
 ln -fs $DOTFILES/.gvimrc ~/.gvimrc
 ln -fs $DOTFILES/.zshrc ~/.zshrc
+ln -fs $DOTFILES/.ycm_extra_conf.py ~/.ycm_extra_conf.py
 ln -fs $DOTFILES/custom ~/.oh-my-zsh/custom
 
