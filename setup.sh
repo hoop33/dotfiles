cf01a015ef9eece1b2aef423bfbde83b15f37aa5
Rob Warner <rob.warner@availity.com>
2015-02-03 15:00:18 -0500

    Remove .gitconfig and revoke github token. Whoops.

--- setup.sh
+++ setup.sh
@@ -4,11 +4,9 @@ DOTFILES=$(pwd)
 
 ln -fs $DOTFILES/.editorconfig ~/.editorconfig
 ln -fs $DOTFILES/.gitattributes ~/.gitattributes
-ln -fs $DOTFILES/.gitconfig ~/.gitconfig
 ln -fs $DOTFILES/.gitignore ~/.gitignore
 ln -fs $DOTFILES/.vimrc ~/.vimrc
 ln -fs $DOTFILES/.gvimrc ~/.gvimrc
 ln -fs $DOTFILES/.zshrc ~/.zshrc
 ln -fs $DOTFILES/.ycm_extra_conf.py ~/.ycm_extra_conf.py
 ln -fs $DOTFILES/custom ~/.oh-my-zsh/custom
-
