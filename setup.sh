5b6151fdb63b1eefeaded31ab3a038be28f4633f
Rob Warner <rwarner@grailbox.com>
2015-03-03 09:32:53 -0500

    Remove .oh-my-zsh/custom before symlinking

--- setup.sh
+++ setup.sh
@@ -8,4 +8,5 @@ ln -fs $DOTFILES/.gitignore ~/.gitignore
 ln -fs $DOTFILES/.vimrc ~/.vimrc
 ln -fs $DOTFILES/.gvimrc ~/.gvimrc
 ln -fs $DOTFILES/.zshrc ~/.zshrc
+rm -rf ~/.oh-my-zsh/custom
 ln -fs $DOTFILES/custom ~/.oh-my-zsh/custom
