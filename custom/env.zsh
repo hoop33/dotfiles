ebf0c46b1526a0e02f9a2461997fefeaaadb8780
Rob Warner <rob.warner@availity.com>
2015-04-30 08:05:12 -0400

    Add support for nvm

--- custom/env.zsh
+++ custom/env.zsh
@@ -10,23 +10,23 @@ fi
 export FIGNORE=".o:~:Application Scripts"
 
 # Java
-if [ -d /Library/Java/JavaVirtualMachines/jdk1.8.0_25.jdk/Contents/Home ]; then
-  export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_25.jdk/Contents/Home
-elif [ -d /Library/Java/JavaVirtualMachines/1.6.0_31-b04-415.jdk/Contents/Home ]; then
-  export JAVA_HOME=/Library/Java/JavaVirtualMachines/1.6.0_31-b04-415.jdk/Contents/Home
-fi
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_25.jdk/Contents/Home
 
 # For AXI builds
-export JAVA3_HOME=$JAVA_HOME
-export JAVA4_HOME=$JAVA_HOME
-export JAVA5_HOME=$JAVA_HOME
-export JAVA6_HOME=$JAVA_HOME
+export JAVA3_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
+export JAVA4_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
+export JAVA5_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
+export JAVA6_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
 export JBOSS_HOME=$HOME/jboss
+export JAVA_CRAP=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
 
 # Gradle
 export GRADLE_OPTS="-Xmx1024m -Xms256m -XX:MaxPermSize=512m -XX:+CMSClassUnloadingEnabled -XX:+HeapDumpOnOutOfMemoryError"
 
 export EDITOR='mvim -f'
+
+export NVM_DIR=$HOME/.nvm
+source $(brew --prefix nvm)/nvm.sh
 
 # For building Lime
 export PKG_CONFIG_PATH=$(brew --prefix python3)/Frameworks/Python.framework/Versions/3.4/lib/pkgconfig:$(brew --prefix qt5)/lib/pkgconfig
