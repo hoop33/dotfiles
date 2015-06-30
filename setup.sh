da87396ab4afd235d6f9d86c02dba623a3bedf66
Rob Warner <rwarner@grailbox.com>
2015-06-30 12:07:39 -0400

    Link atom

--- setup.sh
+++ setup.sh
@@ -6,25 +6,25 @@ FILES=($(find . -maxdepth 1 -type f -name '.*'))
 
 for i in "${FILES[@]}"; do
   FILE=`echo $i | sed -e 's/^..//'`
-  echo Linking $DOTFILES/$FILE to ~/$FILE
-  ln -fs $DOTFILES/$FILE ~/$FILE
+  ln -fsv $DOTFILES/$FILE $HOME/$FILE
 done
 
 # Link the fish config file
-echo Linking fish config
-ln -fs $DOTFILES/config.fish ~/.config/fish/config.fish
+ln -fsv $DOTFILES/config.fish $HOME/.config/fish/config.fish
+
+# Link atom
+echo $DOTFILES/.atom $HOME/.atom
+ln -fsv $DOTFILES/.atom $HOME
 
 # Remove the directory and symlink to ours
-echo Setting up zsh custom directory
-rm -rf ~/.oh-my-zsh/custom
-ln -fs $DOTFILES/custom ~/.oh-my-zsh/custom
+rm -rf $HOME/.oh-my-zsh/custom
+ln -fsv $DOTFILES/custom $HOME/.oh-my-zsh/custom
 
-if [ "$1" == "--node" ]; then
+if [[ "$1" == "--node" ]]; then
   # Install global node modules
   NODE_MODULES=("bower" "david" "gulp" "git://github.com/ramitos/jsctags.git" "jshint" "livedown" "tern")
 
   for i in "${NODE_MODULES[@]}"; do
-    echo Installing $i
     npm install --global $i
   done
 fi
