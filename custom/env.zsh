49358fe86c6886789a05ac70b780e83b5b75fb79
Rob Warner <rob.warner@availity.com>
2015-10-09 09:36:59 -0400

    Add Java 7 and 8 HOME

--- custom/env.zsh
+++ custom/env.zsh
@@ -47,6 +47,8 @@ export JAVA3_HOME=/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
 export JAVA4_HOME=/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
 export JAVA5_HOME=/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
 export JAVA6_HOME=/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
+export JAVA7_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_79.jdk/Contents/Home
+export JAVA8_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home
 export JBOSS_HOME=$HOME/jboss
 
 # Gradle
