a5c9fc1428a2b9500b954a28d52f58e77614f388
Rob Warner <rob.warner@availity.com>
2017-10-20 16:01:41 -0400

    Add fzf c-t

--- zsh_files/env.zsh
+++ zsh_files/env.zsh
@@ -90,6 +90,7 @@ export PATH="$PATH:$HOME/.rvm/bin"
 
 # FZF
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # Eliminate duplicate path entries
 typeset -U PATH
