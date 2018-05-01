a364a5aa7ab23ca8aa35b15a7c089ce131d5535e
Rob Warner <rwarner@grailbox.com>
2018-05-01 16:50:01 -0400

    Add nvm use

--- .zsh_custom/env.zsh
+++ .zsh_custom/env.zsh
@@ -31,6 +31,7 @@ export PATH=$PATH:$HOME/.cargo/bin
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm use 9.6.0 1>/dev/null
 
 # pyenv
 export PYENV_ROOT=$HOME/.pyenv
