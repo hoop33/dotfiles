5f33905965ba6cdd6f5b7624c96e2c0b46e8f1d7
Rob Warner <rwarner@grailbox.com>
2020-07-27 13:53:39 -0400

    feat: add cs3

--- .zsh_custom/functions.zsh
+++ .zsh_custom/functions.zsh
@@ -118,3 +118,16 @@ function pyg() {
 
   pyenv global
 }
+
+function cs3() {
+  local bucket file tmpfile
+
+  bucket=$(aws s3 ls | tr -s ' ' | cut -d' ' -f 3- | fzf)
+  [ -n "$bucket" ] && file=$(aws s3 ls "$bucket" --recursive | tr -s ' ' | cut -d' ' -f 4- | fzf)
+
+  if [ -n "$file" ]; then
+    tmpfile=$(mktemp)
+    aws s3 cp "s3://$bucket/$file" "$tmpfile" && cat "$tmpfile"
+    rm "$tmpfile"
+  fi
+}
