e1ecee8b769dc23eaaa8354561966f9fb2dae855
Rob Warner <rwarner@grailbox.com>
2021-07-01 07:40:39 -0400

    fix: pyenv settings

--- .zsh_custom/env.zsh
+++ .zsh_custom/env.zsh
@@ -58,6 +58,7 @@ export PATH=$PATH:$HOME/wabt/build
 # pyenv
 export PYENV_ROOT=$HOME/.pyenv
 export PATH=$PATH:$PYENV_ROOT/bin
+eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 
 # User configuration
