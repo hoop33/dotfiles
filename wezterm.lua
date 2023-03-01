ceed665dec50251f53df01d515f1ca710c4bc3da
Rob Warner <rwarner@grailbox.com>
2023-03-01 09:26:45 -0500

    feat: set wezterm to tokyo nights and let meta-enter pass to copilot

--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,14 @@
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 
 return {
-  font = wezterm.font("ComicCodeLigatures Nerd Font"),
-  font_size = 14.0,
+	font = wezterm.font("ComicCodeLigatures Nerd Font"),
+	font_size = 14.0,
+	color_scheme = "tokyonight",
+	keys = {
+		{
+			key = "Enter",
+			mods = "META",
+			action = wezterm.action.DisableDefaultAssignment,
+		},
+	},
 }
