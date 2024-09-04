81c482d81c633b487bf07c5e354d271448c7a6bc
Rob Warner <rwarner@grailbox.com>
2024-09-04 15:33:47 -0400

    feat: remove titlebar from wezterm windows

--- wezterm.lua
+++ wezterm.lua
@@ -1,15 +1,19 @@
 local wezterm = require("wezterm")
+local config = wezterm.config_builder()
 
-return {
-	font = wezterm.font("ComicCodeLigatures Nerd Font"),
-	font_size = 14.0,
-	color_scheme = "Galaxy",
-	enable_tab_bar = false,
-	keys = {
-		{
-			key = "Enter",
-			mods = "META",
-			action = wezterm.action.DisableDefaultAssignment,
-		},
-	},
+config.font = wezterm.font({ family = "ComicCodeLigatures Nerd Font" })
+config.font_size = 14.0
+config.color_scheme = "Galaxy"
+config.enable_tab_bar = false
+-- config.window_background_opacity = 0.8
+-- config.macos_window_background_blur = 30
+config.window_decorations = "RESIZE"
+config.keys = {
+  {
+    key = "Enter",
+    mods = "META",
+    action = wezterm.action.DisableDefaultAssignment,
+  },
 }
+
+return config
