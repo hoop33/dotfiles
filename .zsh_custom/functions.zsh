bc45fea4f7df76ab2f9251f42886d43f8dd05bf2
Rob Warner <rwarner@grailbox.com>
2024-09-23 16:30:49 -0400

    chore: run shellcheck on functions and remove unused

--- .zsh_custom/functions.zsh
+++ .zsh_custom/functions.zsh
@@ -1,8 +1,8 @@
 # Open an SQLite file in home directory tree
-function opensql() { find ~ -name $1.sqlite -exec sqlite3 '{}' + ; }
+function opensql() { find ~ -name "$1".sqlite -exec sqlite3 '{}' + ; }
 
 # List path components, one per line
-function path() { echo -e ${PATH//:/\\n}; }
+function path() { echo -e "${PATH//:/\\n}"; }
 
 # Convert hex to decimal
 function h2d() { printf '%d\n' 0x"$1"; }
@@ -14,16 +14,16 @@ function d2h() { printf '%x\n' "$1"; }
 function gbs() {
   local branches branch
   branches=$(git branch | grep -v "^\*") &&
-    branch=$(echo "$branches" | fzf --no-multi) &&
-    git switch $(echo "$branch" | sed "s/.* //")
+  branch=$(echo "$branches" | fzf --no-multi) &&
+  git switch "$(echo "$branch" | xargs)"
 }
 
 # Delete a branch
 function gbd() {
   local branches branch
   branches=$(git branch | grep -v "^\*") &&
-    branch=$(echo "$branches" | fzf --multi) &&
-    git branch -d $(echo "$branch" | sed "s/.* //")
+  branch=$(echo "$branches" | fzf --multi) &&
+  git branch -d $(echo "$branch" | xargs)
 }
 
 # git log --author
@@ -44,14 +44,6 @@ function gsut() {
   git branch --set-upstream-to="origin/$upstream" "$current"
 }
 
-# Accept java version, java --version, and java -version
-#function java() {
-  #case $* in
-    #-v|--version|version) shift 1; command java -version ;;
-    #*) command java "$@" ;;
-  #esac
-#}
-
 # Print out a color table
 function colours() {
   for i in {0..255}; do
@@ -62,7 +54,7 @@ function colours() {
     else
       prefix="  "
     fi
-    printf "\x1b[48;5;${i}m\x1b[38;5;$[255-i]m${prefix}${i} "
+    printf "\x1b[48;5;%dm\x1b[38;5;%dm%s%d " "${i}" "$((255-i))" "${prefix}" "${i}"
     if (((i+1)%16 == 0)); then
       printf "\n"
     fi
@@ -92,7 +84,8 @@ function wh() {
   if [[ $# -eq 0 ]]; then
     echo "usage: wh PORT"
   else
-    PID=$(netstat -vanp tcp | grep "\*\.$1 " | awk '{ print $9 }')
+    local PID
+    PID="$(netstat -vanp tcp | grep "\*\.$1 " | awk '{ print $9 }')"
     if [[ ${PID} -eq 0 ]]; then
       echo "no pid for port $1"
     else
@@ -110,7 +103,7 @@ function fix() {
     echo "usage: fix [bad] [good]"
   else
     local cmd
-    cmd=$(fc -ln -1 | sed -e 's/^ +//' | sed -e "s/$1/$2/")
+    cmd="$(fc -ln -1 | sed -e 's/^ +//' | sed -e "s/$1/$2/")"
     eval "$cmd"
   fi
 }
@@ -122,13 +115,13 @@ function urldecode() {
 
 # Change pyenv global version
 function pyg() {
-  pyenv global $(pyenv versions --bare | fzf)
+  pyenv global "$(pyenv versions --bare | fzf)"
   pyenv global
 }
 
 # cat s3 file
 function s3cat() {
-  local bucket file tmpfile
+  local bucket file
 
   bucket=$(aws s3 ls | tr -s ' ' | cut -d' ' -f 3- | fzf)
   [ -n "$bucket" ] && file=$(aws s3 ls "$bucket" --recursive | tr -s ' ' | cut -d' ' -f 4- | fzf)
@@ -143,28 +136,28 @@ function lw() {
   if [[ $# -eq 0 ]]; then
     echo "usage: lw <executable>"
   else
-    eza -alm $(which "$1")
+    eza -alm "$(which "$1")"
   fi
 }
 
 # Piknik
 # pko <content> : copy <content> to the clipboard
-pko() {
+function pko() {
     echo "$*" | piknik -copy
 }
 
 # pkf <file> : copy the content of <file> to the clipboard
-pkf() {
-    piknik -copy < $1
+function pkf() {
+    piknik -copy < "$1"
 }
 
 # pkfr [<dir>] : send a whole directory to the clipboard, as a tar archive
-pkfr() {
-    tar czpvf - ${1:-.} | piknik -copy
+function pkfr() {
+    tar czpvf - "${1:-.}" | piknik -copy
 }
 
 # Download check
-dlc() {
+function dlc() {
   if [[ $# -ne 3 ]]; then
     echo "usage: dlc <algorithm> <expected> <file>"
   else
@@ -177,200 +170,34 @@ dlc() {
   fi
 }
 
-# SDKMAN
-javad() {
-  sdk default java $(sdk list java | grep 'installed\|local only' | awk '{print $NF}' | fzf)
-}
-
-ai() {
+function ai() {
   local name
   if [[ $# -eq 0 ]]; then
-    name=$(asdf plugin list | fzf)
+    name="$(asdf plugin list | fzf)"
   else
-    name=$1
+    name="$1"
   fi
-  asdf install $name $({ comm -23 <(asdf list all $name | sort --version-sort) <(asdf list $name | awk '{print $1}' | sort --version-sort); echo "latest"; } | fzf)
+  asdf install "$name" "$({ comm -23 <(asdf list all "$name" | sort --version-sort) <(asdf list "$name" | awk '{print $1}' | sort --version-sort); echo "latest"; } | fzf)"
 }
 
-au() {
+function au() {
   local name
   if [[ $# -eq 0 ]]; then
-    name=$(asdf plugin list | fzf)
+    name="$(asdf plugin list | fzf)"
   else
-    name=$1
+    name="$1"
   fi
-  asdf global $name $({ asdf list $name | awk '{print $1}' | grep -v "^\*"; echo "latest"; } | fzf)
-}
-
-# Kitty
-kittycolors() {
-  if [[ $# -eq 0 ]]; then
-    grep -o "#[a-f0-9]\{6\}" ~/.config/kitty/current-theme.conf | pastel color
-  else
-    case $1 in
-      short|--short|-s) for COLOR in $(grep -o "#[a-f0-9]\{6\}" ~/.config/kitty/current-theme.conf); do pastel paint $(pastel textcolor $COLOR) --on $COLOR "$COLOR          "; done ;;
-      *) echo "usage: kittycolors [-s]" ;;
-    esac
-  fi
-}
-
-vcat() {
-  if [[ $# -eq 0 ]]; then
-    echo "usage: vcat <UUID>"
-  else
-    local partition
-    partition="$1"
-
-    local id
-    id=$(vault --bucket availity-data-lake-nonprod-qa-us-east-1 --suppress-header --format csv "$partition" | awk -F "," '{print $1}' | fzf)
-    if [[ $id ]]; then
-      shift
-      vault "$partition/$id" $@
-    fi
-  fi
+  asdf global "$name" "$({ asdf list "$name" | awk '{print $1}' | grep -v "^\*"; echo "latest"; } | fzf)"
 }
 
 # Shell wrapper for `yazi`
 # https://yazi-rs.github.io/docs/quick-start/
 function yy() {
-  local tmp="$(mktemp -t "yazi-cwd.XXXXX")"
-	yazi "$@" --cwd-file="$tmp"
-	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-		cd -- "$cwd"
-	fi
-	rm -f -- "$tmp"
-}
-
-# GitHub Copilot
-ghcs() {
-	FUNCNAME="$funcstack[1]"
-	TARGET="shell"
-	local GH_DEBUG="$GH_DEBUG"
-
-	read -r -d '' __USAGE <<-EOF
-	Wrapper around \`gh copilot suggest\` to suggest a command based on a natural language description of the desired output effort.
-	Supports executing suggested commands if applicable.
-
-	USAGE
-	  $FUNCNAME [flags] <prompt>
-
-	FLAGS
-	  -d, --debug              Enable debugging
-	  -h, --help               Display help usage
-	  -t, --target target      Target for suggestion; must be shell, gh, git
-	                           default: "$TARGET"
-
-	EXAMPLES
-
-	- Guided experience
-	  $ $FUNCNAME
-
-	- Git use cases
-	  $ $FUNCNAME -t git "Undo the most recent local commits"
-	  $ $FUNCNAME -t git "Clean up local branches"
-	  $ $FUNCNAME -t git "Setup LFS for images"
-
-	- Working with the GitHub CLI in the terminal
-	  $ $FUNCNAME -t gh "Create pull request"
-	  $ $FUNCNAME -t gh "List pull requests waiting for my review"
-	  $ $FUNCNAME -t gh "Summarize work I have done in issues and pull requests for promotion"
-
-	- General use cases
-	  $ $FUNCNAME "Kill processes holding onto deleted files"
-	  $ $FUNCNAME "Test whether there are SSL/TLS issues with github.com"
-	  $ $FUNCNAME "Convert SVG to PNG and resize"
-	  $ $FUNCNAME "Convert MOV to animated PNG"
-	EOF
-
-	local OPT OPTARG OPTIND
-	while getopts "dht:-:" OPT; do
-		if [ "$OPT" = "-" ]; then     # long option: reformulate OPT and OPTARG
-			OPT="${OPTARG%%=*}"       # extract long option name
-			OPTARG="${OPTARG#"$OPT"}" # extract long option argument (may be empty)
-			OPTARG="${OPTARG#=}"      # if long option argument, remove assigning `=`
-		fi
-
-		case "$OPT" in
-			debug | d)
-				GH_DEBUG=api
-				;;
-
-			help | h)
-				echo "$__USAGE"
-				return 0
-				;;
-
-			target | t)
-				TARGET="$OPTARG"
-				;;
-		esac
-	done
-
-	# shift so that $@, $1, etc. refer to the non-option arguments
-	shift "$((OPTIND-1))"
-
-	TMPFILE="$(mktemp -t gh-copilotXXX)"
-	trap 'rm -f "$TMPFILE"' EXIT
-	if GH_DEBUG="$GH_DEBUG" gh copilot suggest -t "$TARGET" "$@" --shell-out "$TMPFILE"; then
-		if [ -s "$TMPFILE" ]; then
-			FIXED_CMD="$(cat $TMPFILE)"
-			print -s "$FIXED_CMD"
-			echo
-			eval "$FIXED_CMD"
-		fi
-	else
-		return 1
-	fi
-}
-
-ghce() {
-	FUNCNAME="$funcstack[1]"
-	local GH_DEBUG="$GH_DEBUG"
-
-	read -r -d '' __USAGE <<-EOF
-	Wrapper around \`gh copilot explain\` to explain a given input command in natural language.
-
-	USAGE
-	  $FUNCNAME [flags] <command>
-
-	FLAGS
-	  -d, --debug   Enable debugging
-	  -h, --help    Display help usage
-
-	EXAMPLES
-
-	# View disk usage, sorted by size
-	$ $FUNCNAME 'du -sh | sort -h'
-
-	# View git repository history as text graphical representation
-	$ $FUNCNAME 'git log --oneline --graph --decorate --all'
-
-	# Remove binary objects larger than 50 megabytes from git history
-	$ $FUNCNAME 'bfg --strip-blobs-bigger-than 50M'
-	EOF
-
-	local OPT OPTARG OPTIND
-	while getopts "dh-:" OPT; do
-		if [ "$OPT" = "-" ]; then     # long option: reformulate OPT and OPTARG
-			OPT="${OPTARG%%=*}"       # extract long option name
-			OPTARG="${OPTARG#"$OPT"}" # extract long option argument (may be empty)
-			OPTARG="${OPTARG#=}"      # if long option argument, remove assigning `=`
-		fi
-
-		case "$OPT" in
-			debug | d)
-				GH_DEBUG=api
-				;;
-
-			help | h)
-				echo "$__USAGE"
-				return 0
-				;;
-		esac
-	done
-
-	# shift so that $@, $1, etc. refer to the non-option arguments
-	shift "$((OPTIND-1))"
-
-	GH_DEBUG="$GH_DEBUG" gh copilot explain "$@"
+    local tmp
+    tmp="$(mktemp -t "yazi-cwd.XXXXX")"
+    yazi "$@" --cwd-file="$tmp"
+    if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+    	cd -- "$cwd" || return
+    fi
+    rm -f -- "$tmp"
 }
