da5806371a4541e57e9d7fe4f6bcaa6ab12f185a
Rob Warner <rwarner@grailbox.com>
2026-02-27 10:19:00 -0500

    feat: add window titles to wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -7,7 +7,7 @@ config.color_scheme = "Galaxy"
 config.enable_tab_bar = false
 -- config.window_background_opacity = 0.8
 -- config.macos_window_background_blur = 30
-config.window_decorations = "RESIZE"
+-- config.window_decorations = "RESIZE"
 config.keys = {
   {
     key = "Enter",
